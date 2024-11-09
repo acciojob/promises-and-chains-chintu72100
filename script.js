99fb67289f4f7a26259aa173cb4346a69123d937
Chintu jha <67228053+chintu72100@users.noreply.github.com>
2024-11-09 22:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+let age = document.querySelector("#age");
+let name = document.querySelector("#name");
+let btn = document.querySelector("#btn");
+
+
+
+
+btn.addEventListener("click", (event)=>{
+	event.preventDefault();
+	if(age.value.length === 0 || name.value.length === 0){
+			alert("inputs cannot be empty");
+	}else{
+        promiseResolve();
+	}
+})
+
+function promiseResolve(){
+    new Promise((res,rej)=>{
+        if(age.value > 18){
+            res();
+        }
+        else{
+            rej();
+        }
+    }).then((data)=>{
+        setTimeout(function(){
+            alert(`Welcome ${name.value}. You can vote`);
+        },4000)
+    }).catch((err)=>{
+        alert(`Oh Sorry ${name.value}. You are not old enough`);
+    })
+}
